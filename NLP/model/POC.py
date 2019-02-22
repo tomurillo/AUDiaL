9930027949a181e05cdde99d2e5634c4e82f6957
tomurillo <tomurillo@gmail.com>
2019-02-22 16:44:27 +0100

    Added serialization methods to Query and POC classes

--- NLP/model/POC.py
+++ NLP/model/POC.py
@@ -37,6 +37,72 @@ class POC(object):
             self.end = ann.end
             self.end_original = ann.end
 
+    def overlapsOC(self, oc):
+        """
+        Returns whether this POC overlaps the given Ontology Concept
+        :param oc: SemanticConcept instance
+        :return: True if the OC's text is contained within the POC's, false otherwise.
+        """
+        overlaps = False
+        if oc and oc.OE.annotation.start >= self.start and oc.OE.annotation.end <= self.end:
+            overlaps = True
+        return overlaps
+
+    def to_dict(self):
+        """
+        Converts this POC to an equivalent dictionary (of built-in types) representation
+        :return: dict
+        """
+        d = {'tree': str(self.tree), 'rawText': self.rawText, 'head': str(self.head), 'start': self.start,
+             'end': self.end, 'start_original': self.start_original, 'end_original': self.end_original,
+             'mainSubjectPriority': self.mainSubjectPriority}
+        if self.modifiers:
+            d['modifiers'] = [str(m) for m in self.modifiers]
+        else:
+            d['modifiers'] = None
+        if self.annotation:
+            d['annotation'] = self.annotation.to_dict()
+        else:
+            d['annotation'] = None
+        return d
+
+    def from_dict(self, d):
+        """
+        Populates this instances's attributes from the given dictionary
+        :param d:
+        :return: None; updates current instance
+        """
+        if type(d) is dict:
+            from nltk import Tree
+            from NLP.model.Annotation import Annotation
+            tree = d.get('tree')
+            self.tree = Tree.fromstring(tree) if tree else None
+            self.rawText = d.get('rawText', '')
+            modif = d.get('modifiers', [])
+            self.modifiers = [Tree.fromstring(m) for m in modif]
+            annotation_dict = d.get('annotation')
+            if annotation_dict:
+                ann = Annotation()
+                ann.from_dict(annotation_dict)
+                self.annotation = ann
+            else:
+                self.annotation = None
+            head = d.get('head')
+            if head:
+                try:
+                    self.head = Tree.fromstring(head)
+                except ValueError:  # head is a word (string), not a Tree instance
+                    self.head = head
+            else:
+                self.head = None
+            self.start = d.get('start', -1)
+            self.start_original = d.get('start_original', -1)
+            self.end = d.get('end', -1)
+            self.end_original = d.get('end_original', -1)
+            self.mainSubjectPriority = d.get('mainSubjectPriority', self.MSUB_PRIORITY_MIN)
+        else:
+            raise ValueError('POC.from_dict: parameter must be of type dict.')
+
     def copy(self):
         poc_copy = POC()
         if self.tree:
@@ -80,14 +146,3 @@ class POC(object):
         return poc_copy
 
     __deepcopy__ = deepcopy
-
-    def overlapsOC(self, oc):
-        """
-        Returns whether this POC overlaps the given Ontology Concept
-        :param oc: SemanticConcept instance
-        :return: True if the OC's text is contained within the POC's, false otherwise.
-        """
-        overlaps = False
-        if oc and oc.OE.annotation.start >= self.start and oc.OE.annotation.end <= self.end:
-            overlaps = True
-        return overlaps
--- NLP/model/Query.py
+++ NLP/model/Query.py
@@ -17,7 +17,7 @@ class Query(object):
         self.questionType = QuestionType.VOID
         self.focus = None  # POC instance
         self.pocs = []
-        self.tokens = []
+        self.tokens = []  # List<(string, string)>: list of (word, part-of-speech) tuples of each word in the query
         self.pt = None  # Syntax parse tree (PT) of type nltk.Tree
         self.answerType = None
         self.semanticConcepts = []  # list<list<SemanticConcept>>: overlapped-by-text SCs, first one overlaps the rest
@@ -37,3 +37,74 @@ class Query(object):
                     import sys
                     print('Warning: flattening list of ambiguous SemanticConcepts!', sys.stderr)
         return flat_scs
+
+    def to_dict(self):
+        """
+        Converts this Query instance to an equivalent dictionary (of built-in types) representation
+        :return: dict
+        """
+        d = {'rawQuery': self.rawQuery, 'questionType': self.questionType, 'answerType': self.answerType}
+        if self.focus:
+            d['focus'] = self.focus.to_dict()
+        else:
+            d['focus'] = None
+        d['annotations'] = [ann.to_dict() for ann in self.annotations]
+        d['pocs'] = [p.to_dict() for p in self.pocs]
+        d['tokens'] = [list(t) for t in self.tokens]
+        d['pt'] = str(self.pt) if self.pt else None
+        d['semanticConcepts'] = []
+        for sc_list in self.semanticConcepts:
+            d['semanticConcepts'].append([sc.to_dict() for sc in sc_list])
+        return d
+
+    def from_dict(self, d):
+        """
+        Populates this instances's attributes from the given dictionary
+        :param d:
+        :return: None; updates current instance
+        """
+        if type(d) is dict:
+            from NLP.model.POC import POC
+            from NLP.model.SemanticConcept import SemanticConcept
+            from NLP.model.Annotation import Annotation
+            self.rawQuery = d.get('rawQuery', '')
+            self.questionType = d.get('questionType', QuestionType.VOID)
+            self.answerType = d.get('answerType')
+            focus_dict = d.get('focus')
+            if focus_dict:
+                focus = POC()
+                focus.from_dict(focus_dict)
+                self.focus = focus
+            else:
+                self.focus = None
+            anns = d.get('annotations', [])
+            self.annotations = []
+            for a_dict in anns:
+                a = Annotation()
+                a.from_dict(a_dict)
+                self.annotations.append(a)
+            pocs = d.get('pocs', [])
+            self.pocs = []
+            for p in pocs:
+                poc = POC()
+                poc.from_dict(p)
+                self.pocs.append(poc)
+            tokens = d.get('tokens', [])
+            self.tokens = [tuple(t) for t in tokens]
+            pt = d.get('pt')
+            if pt:
+                from nltk import Tree
+                self.pt = Tree.fromstring(pt)
+            else:
+                self.pt = None
+            scs = d.get('semanticConcepts', [])
+            self.semanticConcepts = []
+            for sc_dict_list in scs:
+                sc_list = []
+                for sc_dict in sc_dict_list:
+                    sc = SemanticConcept()
+                    sc.from_dict(sc_dict)
+                    sc_list.append(sc)
+                self.semanticConcepts.append(sc_list)
+        else:
+            raise ValueError('Query.from_dict: parameter must be of type dict.')
